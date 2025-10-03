61900d2b9d22b98d1852a54becc0d8dea67d8d14
atlinx <atlinuxtm@gmail.com>
2025-10-03 16:42:01 -0400

    Add rollback freshness store

---
+++ src/rollback_freshness_store.cpp
@@ -0,0 +1,69 @@
+#include "rollback_freshness_store.h"
+#include "utils.h"
+
+#include <godot_cpp/core/object.hpp>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/variant/variant.hpp>
+
+bool RollbackFreshnessStore::is_fresh(Node* node, int tick)
+{
+	if(!_data.has(tick))
+	{
+		return true;
+	}
+
+	if(!_data[tick].has(node))
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void RollbackFreshnessStore::notify_processed(Node* node, int tick)
+{
+	if(!_data.has(tick))
+		_data[tick] = HashMap<Node*, bool>();
+
+	_data[tick][node] = true;
+}
+
+void RollbackFreshnessStore::trim()
+{
+	while(!_data.is_empty())
+	{
+		int earliest_tick = 0;
+		bool first = true;
+		for(const KeyValue<int, HashMap<Node*, bool>>& E : _data)
+		{
+			if (first || E.key < earliest_tick)
+			{
+				earliest_tick = E.key;
+				first = false;
+			}
+		}
+
+		if(earliest_tick < (int)Utils::get_autoload("NetworkRollback")->get("history_start"))
+		{
+			_data.erase(earliest_tick);
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void RollbackFreshnessStore::clear()
+{
+	_data.clear();
+}
+
+void RollbackFreshnessStore::_bind_methods() {
+	ClassDB::bind_static_method("RollbackFreshnessStore", D_METHOD("new"), &RollbackFreshnessStore::_new);
+	ClassDB::bind_method(D_METHOD("is_fresh", "node", "tick"), &RollbackFreshnessStore::is_fresh);
+	ClassDB::bind_method(D_METHOD("notify_processed", "node", "tick"), &RollbackFreshnessStore::notify_processed);
+	ClassDB::bind_method(D_METHOD("trim"), &RollbackFreshnessStore::trim);
+	ClassDB::bind_method(D_METHOD("clear"), &RollbackFreshnessStore::clear);
+}
---
+++ src/rollback_freshness_store.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <godot_cpp/godot.hpp>
+#include <godot_cpp/variant/array.hpp>
+#include <godot_cpp/templates/hash_map.hpp>
+#include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/classes/ref_counted.hpp>
+
+using namespace godot;
+
+class RollbackFreshnessStore : public RefCounted {
+	GDCLASS(RollbackFreshnessStore, RefCounted);
+
+protected:
+	HashMap<int, HashMap<Node*, bool>> _data;
+
+	static void _bind_methods();
+
+public:
+	RollbackFreshnessStore() = default;
+	~RollbackFreshnessStore() override = default;
+
+	bool is_fresh(Node* node, int tick);
+	void notify_processed(Node* node, int tick);
+	void trim();
+	void clear();
+	
+	static Ref<RollbackFreshnessStore> _new();
+};
