6ec8e480b116d71e040c8af976dbfa48c3f46d64
atlinx <atlinuxtm@gmail.com>
2025-10-05 01:55:21 -0400

    Remove CPP test, update build yaml

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -5,7 +5,6 @@
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
 
-#include "test.h"
 #include "logger.h"
 #include "property_cache.h"
 #include "property_config.h"
@@ -46,7 +45,6 @@ void initialize_gdextension_types(ModuleInitializationLevel p_level)
 	GDREGISTER_CLASS(_RollbackHistoryRecorder);
 	GDREGISTER_CLASS(_RollbackHistoryTransmitter);
 	GDREGISTER_CLASS(RollbackFreshnessStore);
-	GDREGISTER_CLASS(CPPTest);
 }
 
 void uninitialize_gdextension_types(ModuleInitializationLevel p_level) 
